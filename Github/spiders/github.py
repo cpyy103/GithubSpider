670df8d036d12cb60258a4495f9da7be1fe726dc
Miner127 <1185630378@qq.com>
2019-02-20 09:44:17 +0800

    update

--- Github/spiders/github.py
+++ Github/spiders/github.py
@@ -11,17 +11,19 @@ from Github.settings import USER_AGENTS
 class GithubSpider(scrapy.Spider):
     name = 'github'
     allowed_domains = ['github.com']
-    user = 'ChenPY95'
+    user = 'Miner127'
 
     # 获取用户指定页数的followers
     def get_urls(self, u, *, start=1, stop=2):
         users = []
         for p in range(start, stop+1):
-            response = requests.get(u + '?page=' + str(p) + '&tab=followers',
+            response = requests.get(u + '?page=' + str(p) + '?tab=followers',
                                     headers={'User-Agent': random.choice(USER_AGENTS)})
             root = etree.HTML(response.text)
             for user in root.xpath('//span[@class="link-gray pl-1"]/text()'):
                 users.append(user)
+
+
             try:  # 检查有没有下一页
                 root.xpath('//a[text()="Next"]/@href')[0]
             except:
@@ -33,8 +35,11 @@ class GithubSpider(scrapy.Spider):
     def get_repos(self, u, *, start=1, stop=3):
         repos = []
         for p in range(start, stop+1):
-            response = requests.get(u + '?page=' + str(p) + '&tab=repositories',
-                                    headers={'User-Agent': random.choice(USER_AGENTS)})
+            url = u + '?page='+str(p)+'?tab=repositories'
+            response = requests.get(url,
+                                    headers={'User-Agent': random.choice(USER_AGENTS),
+                                    'Referer:':url})
+
             root = etree.HTML(response.text)
             for repo in root.xpath('//li[@class="col-12 d-block width-full py-4 border-bottom public source"]'):
                 name = repo.xpath('./div/h3/a/@href')[0]
@@ -73,41 +78,48 @@ class GithubSpider(scrapy.Spider):
     def parse(self, response):
         item = GithubItem()
         user = response.url.split('?')[0]
+
         item['name'] = user
         nav = response.xpath('//nav[@class="UnderlineNav-body"]')[0]
 
-        repositories = nav.xpath('./a[@title="Repositories"]/span/text()')[0].extract().split()[0]
+        repositories = nav.xpath('./a[2]/span/text()')[0].extract().split()[0]
         if repositories[-1] == 'k':
             repositories = float(repositories[:-1])*1000
         item['repositories'] = int(repositories)
 
-        stars = nav.xpath('./a[@title="Stars"]/span/text()')[0].extract().split()[0]
+        stars = nav.xpath('./a[4]/span/text()')[0].extract().split()[0]
         if stars[-1] == 'k':
             stars = float(stars[:-1])*1000
         item['stars'] = int(stars)
 
-        followers = nav.xpath('./a[@title="Followers"]/span/text()')[0].extract().split()[0]
+        followers = nav.xpath('./a[5]/span/text()')[0].extract().split()[0]
         if followers[-1] == 'k':
             followers = float(followers[:-1])*1000
         item['followers'] = int(followers)
 
-        followings = nav.xpath('./a[@title="Following"]/span/text()')[0].extract().split()[0]
+        followings = nav.xpath('./a[6]/span/text()')[0].extract().split()[0]
         if followings[-1] == 'k':
             followings = float(followings[:-1])*1000
         item['followings'] = int(followings)
 
         yield item
 
-        for repo in self.get_repos(user):
-            item = GithubRepoItem()
-            item['repo'] = repo[0]
-            item['star'] = repo[1]
-            item['fork'] = repo[2]
-            item['language'] = repo[3]
-            yield item
+        # repos= self.get_repos(user)
+        # print(repos)
+        # for repo in repos:
+        #     item = GithubRepoItem()
+        #     item['repo'] = repo[0]
+        #     item['star'] = repo[1]
+        #     item['fork'] = repo[2]
+        #     item['language'] = repo[3]
+        #     yield item
 
-        for u in self.get_urls(user):
+        users = self.get_urls(user)
+        for u in users:
             yield Request(url='https://github.com/' + u + '?tab=followers',
+                          callback=self.parse, dont_filter=False)
+
+            yield Request(url='https://github.com/' + u + '?tab=following',
                           callback=self.parse, dont_filter=False)
 
 
